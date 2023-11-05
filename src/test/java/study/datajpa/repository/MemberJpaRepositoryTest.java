53404040a8795f097ca4182884d0ba52a9242dde
zangzangs <kimjangh94@gmail.com>
2023-11-06 00:09:05 +0900

    Initial commit- 스프링 데이터 JPA와 DB설정, 동작확인

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,44 @@
+package study.datajpa.repository;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    private MemberJpaRepository memberJpaRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    public void testMember(){
+        Member member = new Member("장장스");
+        Member savedMember = memberJpaRepository.save(member);
+
+        em.flush();
+        em.clear(); //영속성 컨텍스트 초기화
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+        assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
+    }
+
+}
---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package study.datajpa.repository;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import javax.swing.text.html.parser.Entity;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void testMember(){
+        Member member = new Member("장장스");
+        Member savedMember = memberRepository.save(member);
+
+        em.flush();
+        em.clear(); //영속성 컨텍스트 초기화
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+        assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
+    }
+
+}
